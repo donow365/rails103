149df8e19b5e642a0b8a14c507907c94a9727fa4
donow <donow365@gmail.com>
2017-01-20 13:33:52 +0800

    implement groups#update

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -11,7 +11,7 @@ class GroupsController < ApplicationController
  def edit
    @group = Group.find(params[:id])
  end
- 
+
   def new
    @group = Group.new
  end
@@ -22,6 +22,15 @@ class GroupsController < ApplicationController
 
      redirect_to groups_path
  end
+
+ def update
+    @group = Group.find(params[:id])
+
+    @group.update(group_params)
+
+    redirect_to groups_path, notice: "Update Success"
+  end
+
 
  private
 
